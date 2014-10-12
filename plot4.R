1072f5d83827b488d324733eae746c73d61406b0
Serban Tanasa <stanasa@latinumnetwork.com>
2014-10-12 10:38:13 -0400

    plot4 code
    plot4

--- plot4.R
+++ plot4.R
@@ -18,7 +18,18 @@ saveRDS(mydat, "mydat.RDS")
 png("plot4.png") #defaults are 480x480
 par(mfrow=c(2,2))
 plot(mydat$Date, mydat$Global_active_power, type="l",
-     ylab="Global Active Power (kilowatts)", xlab="")
+     ylab="Global Active Power", xlab="")
+plot(mydat$Date, mydat$Voltage, type="l",lwd=0.05,
+     ylab="Voltage", xlab="datetime")
+plot(mydat$Date, mydat$Sub_metering_1, type="l",
+     ylab="Energy sub metering", xlab="")
+     lines(mydat$Date, mydat$Sub_metering_2, col="red")
+     lines(mydat$Date, mydat$Sub_metering_3, col="blue")
+     legend('topright', c("Sub_metering_1","Sub_metering_2",
+                     "Sub_metering_3"), 
+       lty=1, col=c('black','red', 'blue'), cex=.75)
+plot(mydat$Date, mydat$Global_reactive_power, type="l", lwd=0.05,
+     ylab="Global_reactive_power", xlab="datetime")
 dev.off()
 
 
